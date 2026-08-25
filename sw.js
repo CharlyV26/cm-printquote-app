f500333d96687488cb0ad14c52b78fe748bc2a83
CharlyV26 <karloozvm@gmail.com>
2026-08-25 17:08:35 -0600

    Actualización del 25/8/2026

--- sw.js
+++ sw.js
@@ -16,7 +16,7 @@
  * navegador compara este archivo byte a byte y, si sale idéntico, da por hecho
  * que no hay nada nuevo y jamás reemplaza la copia guardada.
  */
-const VERSION = "cm-printquote-20260825230820"
+const VERSION = "cm-printquote-20260825230835"
 const ARCHIVOS = [
   "./",
   "./manifest.webmanifest",
